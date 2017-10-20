075abbabbe7c9e21e78734425d22bc0faca47307
root <root@localhost.localdomain>
2017-10-20 17:29:17 +0900

    first commit

---
+++ client.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <stdio.h>
+
+#define BUFSIZE 1024
+
+using namespace std;
+
+char sendBuf[BUFSIZE];
+char recvBuf[BUFSIZE];
+
+void *recv_chat(void *sd) {
+	int *sock = (int *)&sd;
+	char str[BUFSIZE];
+
+	while(1) {
+		read(*sock, recvBuf, sizeof(recvBuf));	
+		cout << recvBuf << endl;
+
+		if(*recvBuf == '#'){
+			close(*sock);
+			exit(0);
+		}
+	}
+}
+
+
+void *send_chat(void *sd) {
+	int *sock = (int*)&sd;
+	char str[BUFSIZE];
+
+	while(1) {
+		memset(sendBuf, 0, sizeof(sendBuf));
+		//gets(sendBuf);
+		cin >> sendBuf;
+		
+		if(sizeof(sendBuf) > 1)
+			send(*sock, sendBuf, BUFSIZE, 0);
+	}
+
+	cout << "=> Ended Connection...Bye" << endl;
+	exit(1);
+}
+
+int main() {
+	int clientsd;
+	char sendBuf[BUFSIZE];
+	char recvBuf[BUFSIZE];
+	char *ip = "127.0.0.1";
+	int portNum = 6743;
+
+	struct sockaddr_in serverAddr;
+
+	pthread_t recv_th, send_th;
+
+	clientsd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(clientsd == -1) {
+		cout << "\nFailed to create socket client." << endl;
+		exit(1);
+	}
+
+	cout << "\n=> Socket client is created..." << endl;
+
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(portNum);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	if(connect(clientsd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == 0){
+		cout << "=> Connection to the server port: "<< portNum << endl;
+	}
+	
+	memset(recvBuf, 0, sizeof(recvBuf));
+	read(clientsd, recvBuf, sizeof(recvBuf));
+	cout << recvBuf << " Chatting started. " <<endl;
+
+	int status;
+
+	pthread_create(&recv_th, NULL, recv_chat, (void *)clientsd);
+	pthread_create(&send_th, NULL, send_chat, (void *)clientsd);
+
+	pthread_join(recv_th, (void **)status);
+	pthread_join(send_th, (void **)status);
+
+	cout << "=> Ended connection.." << endl;
+	close(clientsd);
+	return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <stdio.h>
+//#include <mysql.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define BUFSIZE 1024
+
+using namespace std;
+pthread_mutex_t mutex_lock; //mutex object 
+
+/* shared resource */
+char sendbuf[BUFSIZE]; //send to all clients
+char recvbuf[BUFSIZE]; //receive from client
+char endbuf[2];
+int cNum; //count client
+int socks[10]; //max client is 10
+
+int listensd = -1;
+
+void *chat(void *sd) {	
+	int *sock = (int *)&sd; 
+	int len;
+	char str[BUFSIZE];
+
+	sprintf(str, "%s %d %s", "\n=> You're < client ", (*sock)+1, ">. ");	
+
+	cout << "New < client " << (*sock)+1 << " > is accessed" << endl;
+	send(socks[*sock], str, sizeof(str), 0);
+	
+	while(1){
+		memset(str, 0, sizeof(str));
+		read(socks[*sock], recvbuf, sizeof(recvbuf));
+
+		cout << "** Client " << (*sock)+1 << ": " << recvbuf << endl;
+	
+		sprintf(str, "%s %d", "Client",(*sock)+1);
+		strcat(str, ": ");
+		strcat(str, recvbuf);
+		for(int i=0 ; i<sizeof(socks) ; i++) {
+			if (i!=(*sock))
+				send(socks[i], str, sizeof(str),0);
+		}
+	}
+}
+
+void *end_chat(void *sd) {
+	int *sock = (int *)&sd;
+	
+	while(1) {
+		gets(endbuf);
+
+		if(*endbuf == '#'){
+			for(int i=0 ; i<sizeof(socks); i++){
+				send(socks[i], endbuf, sizeof(endbuf), 0);
+				close(socks[i]);
+			}
+		}
+		break;
+	}
+	close(listensd);
+	cout << "=> Close Chatting." << endl;
+	exit(0);
+}
+
+int main() {
+//	int listensd; 
+
+	struct sockaddr_in serverAddr; 
+	socklen_t size; 
+	pthread_t thread;
+	pthread_t end_thread;
+
+	listensd = socket(PF_INET, SOCK_STREAM, 0); //IPv4 protocol
+
+	if(listensd == -1) {
+		cout << "\nFailed to create socket server." << endl;
+		exit(1);
+	}
+
+	cout << "\n=> Socket server is created.." << endl;
+
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET; 
+	serverAddr.sin_port = htons(6743); 
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if(bind(listensd, (struct sockaddr*)&serverAddr, sizeof(serverAddr))){
+		cout << "\nFailed to bind server Address." << endl;
+		exit(1);
+	}
+
+	cout << "=> Looking for clients..." << endl; 
+
+	if(listen(listensd, 5) == -1) {
+		cout << "\nListen Error." <<endl;
+		exit(1);
+	}
+
+	socklen_t serverlen = sizeof(serverAddr); //소켓 주소 정보를 포함한 sockaddr구조체를 가리키는 포인터 
+
+	//소켓서버의 address를 serverAddr에 저장
+	getsockname(listensd, (struct sockaddr *)&serverAddr, &serverlen);
+	
+	cout << "=> Server is running on port " << ntohs(serverAddr.sin_port) <<endl;
+
+
+	/******* Chat ******/
+	while(1) {
+		int connectsd;
+		char *ip_addr;
+		
+		struct sockaddr_in clientAddr;
+		size = sizeof(clientAddr);
+
+		connectsd = accept(listensd, (struct sockaddr*)&clientAddr, &size);
+		ip_addr = inet_ntoa(clientAddr.sin_addr);
+						
+		cout << "\n=> New connection from " << ip_addr << endl;
+		
+		socks[cNum++] = connectsd;
+		cout << "=> Total client is " << cNum  << ". "<< endl;
+
+		pthread_create(&thread, NULL, chat, (void *)(cNum-1));
+		pthread_create(&end_thread, NULL, end_chat, (void *)(listensd));
+	}
+	
+	cout << "=> Ended chatting... Bye!" << endl;
+	
+	//close(listensd);
+
+	return 0;
+}
